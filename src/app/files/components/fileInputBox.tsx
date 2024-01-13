3cd67a20806eaee961265f18c547e8278550ce96
Aman Raj Rana <amanrajrana.dev@gmail.com>
2024-01-13 19:23:09 +0530

    Add file submission functionality

--- src/app/files/components/fileInputBox.tsx
+++ src/app/files/components/fileInputBox.tsx
@@ -2,49 +2,117 @@
 
 import { Upload } from "lucide-react";
 import { ChangeEvent, DragEvent, useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import {
+  CustomIconCSV,
+  CustomIconDoc,
+  CustomIconPdf,
+  CustomIconText,
+  CustomIconUnsupported,
+} from "@/svg";
 
-type FileInputBoxProps = {
+type Props = {
+  selectedFile: File | null;
   setSelectedFile: (file: File | null) => void;
 };
 
-export default function FileInputBox({ setSelectedFile }: FileInputBoxProps) {
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file = e.target.files[0];
-      setSelectedFile(file);
-    }
-  };
+export default function FileInputBox({ selectedFile, setSelectedFile }: Props) {
+  const { toast } = useToast();
 
-  const handleDrop = (e: DragEvent<HTMLDivElement>): void => {
+  // STATE VARIABLES
+  const [unsupportedFileType, setUnsupportedFileType] = useState(false);
+
+  const handleDragOver = (e: DragEvent<HTMLSpanElement>) => {
     e.preventDefault();
+    setUnsupportedFileType(false);
+  };
 
-    if (e.dataTransfer.files) {
-      const file = e.dataTransfer.files[0];
-      setSelectedFile(file);
+  const handleDrop = (e: DragEvent<HTMLSpanElement>) => {
+    e.preventDefault();
+    handleFile(e.dataTransfer.files[0]);
+  };
+
+  const handleFile = (file: File) => {
+    // Check if file is not null
+    if (!file) return;
+
+    const fileName = file.name;
+
+    // Check if file type is supported
+    if (
+      !["txt", "pdf", "csv", "doc"].includes(fileName.split(".").pop() || "")
+    ) {
+      setUnsupportedFileType(true);
+      toast({
+        description: "Unsupported File",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    setUnsupportedFileType(false);
+    setSelectedFile(file);
+  };
+
+  const getFileTypeIcon = (fileType: string) => {
+    const height = 200;
+
+    switch (fileType) {
+      case "txt":
+        return <CustomIconText height={height} />;
+      case "pdf":
+        return <CustomIconPdf height={height} />;
+      case "csv":
+        return <CustomIconCSV height={height} />;
+      case "doc":
+        return <CustomIconDoc height={height} />;
+      default:
+        setUnsupportedFileType(true);
+        return <CustomIconUnsupported height={height} />;
     }
   };
+
+  if (selectedFile) {
+    const fileName = selectedFile.name;
+    return (
+      <div className="w-full flex justify-center flex-col">
+        {getFileTypeIcon(fileName.split(".").pop() || "")}
+        <p className="text-center">{fileName}</p>
+      </div>
+    );
+  }
 
   return (
     <>
       <label
         htmlFor="file"
-        className="border-2 border-dashed py-16 px-8 flex justify-center items-center flex-col gap-y-1.5 cursor-pointer hover:bg-gray-50 transition-colors duration-300"
-        onDragOver={(e: DragEvent) => e.preventDefault()}
-        onDrop={() => handleDrop}
+        className={`${
+          unsupportedFileType ? "border-red-400 bg-red-100" : ""
+        } border-2 border-dashed py-16 px-8 flex justify-center items-center flex-col gap-y-1.5 cursor-pointer hover:bg-gray-50 transition-colors duration-300`}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
       >
         <Upload />
         <p>Drag & drop here, or click to select files</p>
         <p className="text-gray-600 text-sm">
-          Supported File Types: .pdf, .txt
+          Supported File Types: .pdf, .doc, .txt and .csv
         </p>
       </label>
+
       <input
-        onChange={handleFileChange}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          e.target.files && handleFile(e.target.files[0])
+        }
         type="file"
         className="hidden"
         id="file"
-        accept=".pdf,.txt,.csv"
+        accept=".pdf,.txt,.csv,.doc"
       />
+      {unsupportedFileType && (
+        <p className="text-red-500 text-sm">
+          Unsupported file type. Choose only .pdf, .doc, .txt and .csv
+        </p>
+      )}
     </>
   );
 }
--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -4,28 +4,100 @@ import { useState } from "react";
 import FileInputBox from "./components/fileInputBox";
 import { Button } from "@/components/ui/button";
 import { Loader2Icon, RotateCcw, Upload } from "lucide-react";
+import { toast } from "@/components/ui/use-toast";
+import {
+  handleSaveFileDataToDB,
+  handleSaveResponseToDB,
+  handleUploadFileToOpenAI,
+} from "@/lib/handleUploadFile";
 
 export default function FileUploadPage() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+
+  const handleReset = () => {
+    toast({ description: "File reset" });
+    setSelectedFile(null);
+  };
+
+  const getFileAsBase64 = async (file: File) => {
+    const reader = new FileReader();
+    return new Promise<string>((resolve, reject) => {
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        if (e.target?.result) {
+          resolve(e.target.result as string);
+        } else {
+          reject(new Error("FileReader failed to load file"));
+        }
+      };
+
+      reader.onerror = (e) => {
+        reject(new Error("FileReader encountered an error while reading file"));
+      };
+
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const handleSubmission = async () => {
+    console.log("Submitting file");
+    if (!selectedFile) return;
+
+    setLoading(true);
+
+    try {
+      const fileName = selectedFile.name;
+      const fileType = selectedFile.type;
+
+      // Saving file to DB
+      const fileData = await getFileAsBase64(selectedFile);
+
+      // Uploading file to OpenAI
+      const data = new Blob([selectedFile], { type: selectedFile.type });
+
+      const [response] = await Promise.all([
+        handleUploadFileToOpenAI(data, fileName),
+        handleSaveFileDataToDB(fileData, fileType, fileName),
+      ]);
+
+      await handleSaveResponseToDB(response.data);
+    } catch (error: SuppressedError | any) {
+      toast({
+        title: error?.status || "Error",
+        description: error.message || "Error uploading file",
+        variant: "destructive",
+      });
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
 
   return (
     <main className="container">
       <div className="border rounded mt-32">
         <h1 className="border-b px-8 py-4 text-xl font-semibold">Files</h1>
         <div className="p-8">
-          <FileInputBox setSelectedFile={setSelectedFile} />
+          <FileInputBox
+            selectedFile={selectedFile}
+            setSelectedFile={setSelectedFile}
+          />
         </div>
       </div>
       <div className="flex justify-end gap-2 flex-wrap-reverse my-4">
         <Button
-          disabled={!selectedFile}
+          disabled={!selectedFile || loading}
           className="w-full gap-1 sm:w-24"
           variant={"outline"}
+          onClick={handleReset}
         >
           <RotateCcw size={16} /> Reset
         </Button>
-        <Button disabled={!selectedFile} className="w-full gap-1 sm:w-24">
+        <Button
+          disabled={!selectedFile || loading}
+          className="w-full gap-1 sm:w-24"
+          onClick={handleSubmission}
+        >
           {loading ? (
             <>
               <Loader2Icon className="animate-spin" />
